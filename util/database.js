45c20e80bcd1a2695aaf8a3b7213756c84ec0034
Piyush7034 <piyushshukla2100@gmail.com>
2023-09-21 20:02:50 +0530

    Added user in mongodb

--- util/database.js
+++ util/database.js
@@ -1,27 +1,32 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://piyushshukla79:0mZ8bhOXAIM7joW9@cluster0.xt1p8da.mongodb.net/shop?retryWrites=true&w=majority')
-  .then(client => {
-    console.log('Connected!');
-    _db = client.db();
-    callback();
-  })
-  .catch(err => {
-    console.log(err);
-    throw err;
-  });
+  MongoClient.connect(
+    "mongodb+srv://piyushshukla79:wscEcJvLR0onzCCN@cluster0.xt1p8da.mongodb.net/shop?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+    }
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
 const getDb = () => {
-  if(_db) {
+  if (_db) {
     return _db;
   }
-  throw 'No database found!';
-}
+  throw "No database found!";
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;

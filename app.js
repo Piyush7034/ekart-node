259d5d847639ffddf82c2a7d2cd2f58abca381b5
Piyush7034 <piyushshukla2100@gmail.com>
2023-10-07 00:36:28 +0530

    Added sessions

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://piyushshukla79:node-complete@cluster0.xt1p8da.mongodb.net/shop?retryWrites=true&w=majority";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,15 +27,14 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use((req, res, next) => {
-  User.findById("650fe116f14abfea827009ac")
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -35,18 +43,16 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://piyushshukla79:node-complete@cluster0.xt1p8da.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then(user => {
-      if(!user) {
+    User.findOne().then((user) => {
+      if (!user) {
         const user = new User({
-          name: 'Piyush',
-          email: 'piyush@test.com',
+          name: "Piyush",
+          email: "piyush@test.com",
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
